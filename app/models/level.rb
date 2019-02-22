2ddad7be330ce37db1e9f8933957d1c710732ffc
Adam Ruzicka <aruzicka@redhat.com>
2019-02-22 15:57:51 +0100

    Replace severity with level

--- app/models/level.rb
+++ app/models/level.rb
@@ -7,4 +7,6 @@ class Level < ApplicationRecord
                    :uniqueness => { :scope => :event_type_id }
   validates :external_id, :presence => true,
                           :uniqueness => { :scope => :event_type_id }
+  has_many :level_filters
+  has_many :filters, :through => :level_filters, :inverse_of => :levels
 end
