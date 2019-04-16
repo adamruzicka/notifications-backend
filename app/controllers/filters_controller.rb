592485b0e5484e9aa757a8e6f24b4885feec59fc
Adam Ruzicka <aruzicka@redhat.com>
2019-04-16 11:14:45 +0200

    Fix routes

--- app/controllers/filters_controller.rb
+++ app/controllers/filters_controller.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class FiltersController < ApplicationController
-  before_action :find_filter, :only => %i[destroy update show]
-
   def index
     process_index Filter, FilterSerializer
   end
--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
     end
 
     resources :endpoints, :except => %(edit new) do
-      resource :filter, :only => %(index)
+      resources :filters, :only => %(index)
       member do
         post 'test', to: 'endpoints#test'
       end
