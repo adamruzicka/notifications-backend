d02ab5da1ae4069db82895135b924b248b392332
Adam Ruzicka <aruzicka@redhat.com>
2019-06-06 14:32:17 +0200

    Collect resque metrics

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,4 +1,28 @@
 # frozen_string_literal: true
 
+require 'notifications'
+
 class ApplicationJob < ActiveJob::Base
+  around_perform :handle_prometheus
+
+  def handle_prometheus
+    success = false
+    shutdown = false
+    start = ::Process.clock_gettime(::Process::CLOCK_MONOTONIC)
+    result = yield
+    success = true
+    result
+  rescue Notifications::RecoverableError => e
+    shutdown = true
+    raise e
+  ensure
+    duration = ::Process.clock_gettime(::Process::CLOCK_MONOTONIC) - start
+    PrometheusExporter::Client.default.send_json(
+        type: 'resque',
+        name: self.class.name,
+        success: success,
+        shutdown: shutdown,
+        duration: duration
+    )
+  end
 end
